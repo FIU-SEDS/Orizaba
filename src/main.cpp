d0e54da8c7f9b493bd63048b00db0011340d1123
Tomas Mejia <tmeji009@fiu.edu>
2025-05-06 00:40:10 -0400

    initialized Lora 998

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,22 @@
-#include <state_manager.h>
-#include <common_sensors.h>
+// #include <state_manager.h>
+// #include <common_sensors.h>
 
+#include <Arduino.h>
 void setup()
 {
-  Serial.begin(BAUD_RATE);
-  Wire.begin();
+  Serial.begin(115200);
 
-  pinMode(13, OUTPUT);
+    
+    Serial.println("AT+MODE = 0");
+    delay(1000);
+    Serial.println("AT+ADDRESS = 1"); // transmitter address 1, ground station address 2 
+    delay(1000);
+    Serial.println("AT+BAND = 915000000");
+    delay(1000);
+    Serial.println("AT+IPR = 115200");
+    delay(1000);
 }
-
-void loop()
-{
-  digitalWrite(13, HIGH);
-  delay(500);
-  digitalWrite(13, LOW);
-  delay(500);
+void loop() {
+Serial.println("AT+SEND = 2,2,Hi");
+delay(200);
 }
