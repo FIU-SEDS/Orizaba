2a3077db82dca9ad705c0c405398070b31bbeb50
Tomas Mejia <tmeji009@fiu.edu>
2025-05-07 21:38:39 -0400

    minor changes

--- src/main.cpp
+++ src/main.cpp
@@ -7,16 +7,16 @@ void setup()
   Serial.begin(115200);
 
     
-    Serial.println("AT+MODE = 0");
+    Serial.println("AT+MODE=0");
     delay(1000);
-    Serial.println("AT+ADDRESS = 1"); // transmitter address 1, ground station address 2 
+    Serial.println("AT+ADDRESS=1"); // transmitter address 1, ground station address 2 
     delay(1000);
-    Serial.println("AT+BAND = 915000000");
+    Serial.println("AT+BAND=915000000");
     delay(1000);
-    Serial.println("AT+IPR = 115200");
+    Serial.println("AT+IPR=15200");
     delay(1000);
 }
 void loop() {
-Serial.println("AT+SEND = 2,2,Hi");
+Serial.println("AT+SEND=2,2,Hi");
 delay(200);
 }
