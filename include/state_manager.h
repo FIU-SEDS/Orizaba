489da01901d1d1aedd5ed0d6a0b1376ec370b2c9
Nathan <martineznathan1010@gmail.com>
2025-06-05 02:41:13 -0400

    setup main.cpp's setup and loop.

--- include/state_manager.h
+++ include/state_manager.h
@@ -24,9 +24,13 @@ enum rocket_state : uint8_t
   LANDED
 };
 
+extern enum rocket_state rocket;
+
 // State manager functions
 bool initialize_all_components(); // if one sensor's power on function returns false then it will reset the teensy 4.1
 
 void update_rocket_states(rocket_state state); // determines which state the rocket is in and prioritizes said sensor 
+
+bool run_priority_sensor(rocket_state rs);
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,17 @@
 #include <log.h>
 #include <Arduino.h>
 #include <string>
+#include <state_manager.h>
+#include <io.h>
 
 void setup()
 {
+  initialize_all_components();
 }
 
 void loop()
 {
+  update_rocket_states(rocket);
+  run_priority_sensor(rocket);
+  log_current_time();
 }
