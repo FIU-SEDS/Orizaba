dcf4fe6d27d7f599a883410c5ff0976dcaea33af
Nathan <martineznathan1010@gmail.com>
2025-06-05 01:57:49 -0400

    fixed issue with log.

--- src/log.cpp
+++ src/log.cpp
@@ -1,13 +1,11 @@
 #include <Arduino.h>
 #include <string>
 
-inline void _debug_log(const char* file, int line, const char* str) {
+void _debug_log(const char* file, int line, const char* str) 
+{
   #if DEBUG == 1
   std::string s = std::string("[") + file + ":" + std::to_string(line) + "] " + str;
   Serial.print(s.c_str());
   #endif
   (void)file, (void)line, (void)str; // this is to silence potential compiler errors.
 }
-
-#define debug_log(_log_str) \
-_debug_log(__FILE__, __LINE__, __log_str);
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include <state_manager.h>
 #include <common_sensors.h>
+#include <log.h>
+#include <Arduino.h>
+#include <string>
 
-void setup() {}
+static enum flight_state fs = IDLE;
+
+void setup() {
+  initialize_all_components();
+  SCB_AIRCR = 0x05FA0004; // should reset tensy4.1
+}
 
 void loop()
 {
+  check_states
+  SCB_AIRCR = 0x05FA0004; // should reset tensy4.1
 }
