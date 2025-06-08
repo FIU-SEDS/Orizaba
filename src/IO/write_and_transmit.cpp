53733dd52a90b14e17f0f18d83ad745e811a59da
Nathan <martineznathan1010@gmail.com>
2025-06-08 00:30:10 -0400

    added transmit_data function to write_and_transmit func

--- src/IO/write_and_transmit.cpp
+++ src/IO/write_and_transmit.cpp
@@ -1,20 +1,25 @@
 #include <io.h>
 
 bool write_and_transmit(enum sensors SID, float x) {
-  unsigned char b_arr[1 + sizeof(x)] = {SID};
-  unsigned char *ptr = (unsigned char*) &x;
-  
-  for(std::size_t i = 0; i < sizeof(x); i++) {
-    b_arr[i + 1] = ptr[i];
-  }
+  unsigned char b_arr[1 + sizeof(float)] = {SID};
+  memcpy(b_arr + 1, (unsigned char*) &x, sizeof(float));
 
   store_data(b_arr, sizeof(b_arr));
+  transmit_data((char*)b_arr, sizeof(b_arr));
 
-  return false;
+  return true;
 }
 
 bool write_and_transmit(enum sensors SID, serial &s) {
-  s.ser.insert(s.ser.begin(), SID);
+  s.ser.insert(s.ser.begin(), SID); // insert the header id in front of the data.
+  
   store_data(&s.ser[0], s.ser.size());
-  return false;
+  if(SID == IMUS) { // WE DO NOT WANT TO SEND ALL THE IMU'S DATA OVER RADIO! WE ONLY WANT TO SEND SOME OF THE DATA
+    transmit_data(&s.ser[7 * sizeof(float)], 
+                  8 * sizeof(float));
+    return true;
+  }
+  transmit_data(&s.ser[0], s.ser.size());
+
+  return true;
 }
