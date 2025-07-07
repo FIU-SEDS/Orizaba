b5de1211fd6fad6f2066b0b80559badaf0e76f98
Eriel Cabrera <erielcabrera1234@gmail.com>
2025-07-06 20:44:52 -0600

    added arguement during object initlization that uses Wire2

--- src/test.cpp
+++ src/test.cpp
@@ -1,28 +1,14 @@
 #include <Wire.h>
+#include <common_sensors.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
-/* This driver reads raw data from the BNO055
-
-   Connections
-   ===========
-   Connect SCL to analog 5
-   Connect SDA to analog 4
-   Connect VDD to 3.3V DC
-   Connect GROUND to common ground
-
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
-*/
-
-/* Set the delay between fresh samples */
 #define BNO055_SAMPLERATE_DELAY_MS (100)
 
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
 //                                   id, address
-Adafruit_BNO055 bno = Adafruit_BNO055(-1, 0x28, &Wire2);
+Adafruit_BNO055 bno = Adafruit_BNO055(-1, MAIN_IMU_ADDRESS, &Wire2);
 
 /**************************************************************************/
 /*
